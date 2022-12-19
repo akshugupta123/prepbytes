6d520196ac2aca31af4d177c540c25964b70651a
akshugupta123 <akshathagupta888@gmail.com>
2022-12-19 17:52:07 +0530

    fifth

---
+++ javacoding/src/javacoding_package/Lastdigitnum.java
@@ -0,0 +1,21 @@
+/*Take an integer as input from the user and print last digit of the number num=4567*/
+/*last digit=7
+ * if taken 43 number 3 is the last digit 3
+ * divisible 4567 by x such that the remainder 7 */
+
+package javacoding_package;
+import java.util.*;
+import java.io.*;
+
+public class Lastdigitnum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=0,last_digit;
+		Scanner sc=new Scanner(System.in);
+		last_digit=num %10;
+		System.out.println(last_digit);		
+
+	}
+
+}
