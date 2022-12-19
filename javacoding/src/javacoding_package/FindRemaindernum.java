9adbe1c7102adfd2138d827f1eb7af32c6f7688e
akshugupta123 <akshathagupta888@gmail.com>
2022-12-19 17:20:23 +0530

    fouth

---
+++ javacoding/src/javacoding_package/FindRemaindernum.java
@@ -0,0 +1,25 @@
+/*what a program to take a num as input from the user and print the remainder when the no is divided by 10
+ * n=456
+ * divided 456by 6
+ * quot=10
+ * remainde=45
+ * remainder = 6
+ * divis operator /
+ * module operator % */
+
+package javacoding_package;
+import java.util.*;
+import java.io.*;
+public class FindRemaindernum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int rem;
+		int n = 0;
+		Scanner sc=new Scanner(System.in);
+		rem = n%10;
+		System.out.println(rem);
+
+	}
+
+}
