737d231a98acbbf50c868af6879debf3628416c6
akshugupta123 <akshathagupta888@gmail.com>
2022-12-20 16:25:32 +0530

    module

---
+++ javacoding/src/javacoding_package/ModulusTen.java
@@ -0,0 +1,17 @@
+package javacoding_package;
+import java.util.*;
+import java.io.*;
+
+public class ModulusTen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sc=new Scanner(System.in);
+	      int a;
+	       a=sc.nextInt();
+	      // b=sc.nextInt();
+	      System.out.println(a%10);
+
+	}
+
+}
