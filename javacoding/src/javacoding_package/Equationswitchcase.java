94079744c8c4286158ce8db6b3d9d4ee99895a66
akshugupta123 <akshathagupta888@gmail.com>
2022-12-24 20:52:33 +0530

    2*n/3switch case checking the condition

---
+++ javacoding/src/javacoding_package/Equationswitchcase.java
@@ -0,0 +1,27 @@
+package javacoding_package;
+import java.util.*;
+import java.io.*;
+
+public class Equationswitchcase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=3;
+		switch(2*n/3)
+		{
+		case 1:
+			System.out.println(1);
+			break;
+		case 2:
+			System.out.println(2);
+			break;
+		case 3:
+			System.out.println(3);//its executed the case3 its true condtion inthis switch case
+			break;
+		default:
+			System.out.println(n);
+		}
+
+	}
+
+}
