60a488676057e72e41298cf4a888adbb30e733a0
akshugupta123 <akshathagupta888@gmail.com>
2022-12-24 20:50:05 +0530

    switch case using n==3 statement executed

---
+++ javacoding/src/javacoding_package/Switchstatement.java
@@ -0,0 +1,30 @@
+//switch statement using integer num is equal or not checking the condition
+
+package javacoding_package;
+import java.util.*;
+import java.io.*;
+
+public class Switchstatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=3;
+		switch(n)
+		{
+		case 1:
+			System.out.println(1);
+			break;
+		case 2:
+			System.out.println(2);
+			break;
+		case 3:
+			System.out.println(3);//its executed the case3 its true condtion inthis switch case
+			break;
+		default:
+			System.out.println(n);
+			
+		}
+
+	}
+
+}
